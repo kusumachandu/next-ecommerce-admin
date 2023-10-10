c872b394025b371987778f2674b203f0ed297e35
chandra shekar <kusumachandu43@gmail.com>
2023-10-10 14:51:04 +0530

    modal component created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
+"use client"
+
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { Modal } from '@/components/ui/modal'
 import { UserButton } from '@clerk/nextjs'
+import { title } from 'process'
 
 export default function SetupPage() {
   return (
     <div className='p-4'>
       <UserButton afterSignOutUrl='/' />
+      <div>
+        <Modal isOpen onClose={() => {}} title='title' description='first test case'>
+          children
+        </Modal>
+      </div>
     </div>
   )
 }
