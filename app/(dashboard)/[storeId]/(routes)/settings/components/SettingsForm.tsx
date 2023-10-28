587f56ae3d332705fb632fc5f47cde638ca06084
chandra shekar <kusumachandu43@gmail.com>
2023-10-28 14:22:00 +0530

    created patch and delete route for store updates

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -1,33 +1,101 @@
 "use client"
 
-import { Button } from '@/components/ui/button';
-import { Heading } from '@/components/ui/heading';
+import * as z from "zod"
+import React, { useState } from 'react'
 import { Store } from '@prisma/client';
 import { Trash } from 'lucide-react';
-import React from 'react'
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from '@/components/ui/button';
+import { Heading } from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
+const formSchema = z.object({
+  name: z.string().min(1),
+})
+
+type SettingsFormValues = z.infer<typeof formSchema>
+
 export const SettingsForm: React.FC<SettingsFormProps> = ({
   initialData
 }) => {
+
+  const params = useParams();
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
+  });
+
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success('store updated')
+    } catch (error) {
+      toast.error('something went wrong')
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <>
-    <div className='flex items-center justify-between'>
-      <Heading 
-        title = 'Settings'
-        description= 'Manage Store Preferences'
-      />
-      <Button
-        variant='destructive'
-        size='sm'
-        onClick={() => {}}
-      >
-        <Trash className='h-4 w-4' />
-      </Button>
-    </div>
+      <div className='flex items-center justify-between'>
+        <Heading 
+          title = 'Settings'
+          description= 'Manage Store Preferences'
+        />
+        <Button
+          disabled={loading}
+          variant='destructive'
+          size='sm'
+          onClick={() => setOpen(true)}
+        >
+          <Trash className='h-4 w-4' />
+        </Button>
+      </div>
+      <Separator className='mt-2 border' />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className=" w-full">
+          <div className="grid grid-cols-3 gap-8">
+            <FormField 
+              control={form.control} 
+              name="name" 
+              render={({ field }) => {
+                return (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input disabled={loading} placeholder="Store Name" {...field }/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+                )
+              }}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto mt-4" type="submit">
+            Save Changes
+          </Button>
+        </form>
+      </Form>
     </>
   )
 }
